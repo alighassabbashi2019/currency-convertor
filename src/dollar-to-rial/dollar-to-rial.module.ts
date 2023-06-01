5ce130bca8357b3ad15add7a170d1ef803a1ac24
mhvn <mhvn092@gmail.com>
2023-06-01 13:51:34 +0330

    initial commit

--- src/dollar-to-rial/dollar-to-rial.module.ts
+++ src/dollar-to-rial/dollar-to-rial.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { CurrencyConvertorModule } from '../currency-convertor/currency-convertor.module';
 import { DollarToRialService } from './dollar-to-rial.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LoggingInterceptor } from '../interceptors/aligh.interceptor';
 
 @Module({
     imports: [
         CurrencyConvertorModule.forFeature({from: 'Dollar'})
     ],
-    providers: [DollarToRialService, {provide: APP_INTERCEPTOR, useClass: LoggingInterceptor}],
+    providers: [DollarToRialService],
     controllers: []
 })
 export class DollarToRialModule {}
